bd0b06b0d9ac85e26f1f8d0a9897f98619cbc0ea
Tom Berghuis <thomas.berghuis@gmail.com>
2019-03-01 16:20:18 +1100

    .

--- src/js/background.js
+++ src/js/background.js
@@ -3,9 +3,9 @@ import '../img/icon-34.png';
 
 import 'chrome-extension-async';
 
-// chrome.browserAction.onClicked.addListener(function(tab) {
-// 	chrome.runtime.openOptionsPage();
-// });
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.runtime.openOptionsPage();
+});
 
 // chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 // 	console.log('request,sender', request, sender);
