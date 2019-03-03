51af4694bb64730ef9b7f4e5c3ec29c8bdcdfc4b
Tom Berghuis <thomas.berghuis@gmail.com>
2019-03-04 09:53:35 +1100

    .

--- src/js/components/AddAlert.jsx
+++ src/js/components/AddAlert.jsx
@@ -4,10 +4,41 @@ import { Button, Container, Divider, Header, Input, Form } from 'semantic-ui-rea
 import AllPairings from '../constants/allPairings';
 
 class AddAlert extends React.Component {
+	state = {
+		pairingFieldError: false,
+		priceFieldError: false
+	};
+
 	addAlertListClickHandler = () => {
 		console.log('clicked');
-		console.log('pairing', this.pairing.value);
-		console.log('price', this.price.value);
+		// console.log('AllPairings', AllPairings);
+		// console.log('pairing', this.pairing.value);
+		// console.log('price', this.price.value);
+		// console.log('price', parseFloat(this.price.value));
+
+		let formValid = true;
+
+		// validate price
+		if (isNaN(parseFloat(this.price.value))) {
+			this.setState({ priceFieldError: true });
+			formValid = false;
+		} else {
+			this.setState({ priceFieldError: false });
+		}
+
+		// validate pairing
+		let pairingValueUpper = this.pairing.value.toUpperCase();
+		if (AllPairings.indexOf(pairingValueUpper) === -1) {
+			this.setState({ pairingFieldError: true });
+			formValid = false;
+		} else {
+			this.setState({ pairingFieldError: false });
+		}
+
+		// return if invalid pairing or price
+		if (!formValid) {
+			return;
+		}
 	};
 
 	render() {
@@ -16,11 +47,11 @@ class AddAlert extends React.Component {
 				<Header as="h1">Add Alert</Header>
 
 				<Form>
-					<Form.Field>
+					<Form.Field error={this.state.pairingFieldError}>
 						<label>Pairing</label>
 						<input ref={(input) => (this.pairing = input)} placeholder="ETHBTC" />
 					</Form.Field>
-					<Form.Field>
+					<Form.Field error={this.state.priceFieldError}>
 						<label>Price</label>
 						<input ref={(input) => (this.price = input)} placeholder="0.1" />
 					</Form.Field>
