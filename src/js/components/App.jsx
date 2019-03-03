05091a883da02bc8cade8d7e2a8440324341b9d3
Tom Berghuis <thomas.berghuis@gmail.com>
2019-03-03 11:40:24 +1100

    .

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import icon from "../../img/icon-128.png"
-import { hot } from "react-hot-loader";
 
 class App extends React.Component {
   render () {
     return (
       <div>
-          App
+          <h1>Add Alert</h1>
       </div>
     )
   }
 };
-
-// export default hot(App)
 
 export default App
