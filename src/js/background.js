26b44b8125201422bc392133576a4af356aabd52
Tom Berghuis <thomas.berghuis@gmail.com>
2019-03-05 13:02:20 +1100

    .

--- src/js/background.js
+++ src/js/background.js
@@ -12,35 +12,14 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log('request,sender', request, sender);
-	// if (request.getprice && Object.entries(allCurrentPrices).length !== 0) {
-	// 	// const price = allCurrentPrices.find((x) => x.symbol === request.getprice).price;
-	// 	// console.log('price', price);
-	// 	sendResponse(allCurrentPrices[request.getprice]);
-	// }
+	// console.log('request,sender', request, sender);
 
 	// this is so client does not need to wait till next interval for update on options.html page load
 	if (request.getAllCurrentPrices) {
 		sendResponse();
-		getAllCurrentPrices();
+		chrome.runtime.sendMessage({ updatedAllCurrentPrices: allCurrentPrices });
 	}
 });
-
-// If you want to use the same callback you can use Promise syntax too:
-
-// setTimeout(async () => {
-// 	try {
-// 		await chrome.runtime.sendMessage({ messsage: 'form background' });
-
-// 		// await chrome.storage.sync.set({ key: "value" });
-
-// 		chrome.storage.sync.set({ key: 'value' }, function() {
-// 			console.log('Value is set to ' + 'value');
-// 		});
-// 	} catch (e) {
-// 		console.log('error', e);
-// 	}
-// }, 10000);
 
 // setTimeout(async () => {
 // 	try {
